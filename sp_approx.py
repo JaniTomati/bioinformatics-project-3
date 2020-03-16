1a3b251e381266e7d0142f3b7e702b5bf5cf7c0f
Jana Puschmann <jana.puschmann@post.au.dk>
2020-03-16 17:02:16 +0100

    Add verification to Maries algorithm

--- sp_approx.py
+++ sp_approx.py
@@ -1,304 +1,325 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# python3 2_approximation_algorithm.py -c scoring_matrix.txt -s testdata_short.fasta
 
-
+import time
 import argparse
 import numpy as np
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
+# Example call
+# python3 sp_exact3.py --C scoring_matrix.txt --P Y Seqs.fasta
+
 # Default values
 cost = np.array([
-    # A  C  G  T
-    [0, 5, 2, 5],
-    [5, 0, 5, 2],
-    [2, 5, 0, 5],
-    [5, 2, 5, 0]])
+		[0, 5, 2, 5, 5],  # A
+		[5, 0, 5, 2, 5],  # C
+		[2, 5, 0, 5, 5],  # G
+		[5, 2, 5, 0, 5],  # T
+		[5, 5, 5, 5, 0]])  #-'
+
+look_up = {'a':0, 'c':1, 'g':2, 't':3, 'A':0, 'C':1, 'G':2, 'T':3, '-':4, 'N':0, 'R':0, 'S':0}
+
 gap_cost = 5
-alphabet = ["A", "C", "G", "T"]
-look_up = {"A": 0, "C": 1, "G": 2, "T": 3, "N": 0, "R": 0, "S": 0}
 
 
-def print_usage():
-    """ Define a usage message """
-    print("""
-          Usage: sp_approx.py -s <sequence_file> -c <cost_file>\n
-          where <sequence_file> contains a set of sequences over the alphabet
-          {A,C,G,T,-} in FASTA format.
-          And <cost_file> contains a cost matrix and a gap cost in a phylip-like
-          format. If -c is not specified, the default values are used.
-          """)
+
+# Load scoring matrix
+def read_in_scoring_matrix():
+	""" Read in the alphabet, scoring matrix and gap cost """
+	alphabet_size = None
+	scoring_matrix = []
+	alphabet = []
+
+	with open(file, "r") as f:
+		lines = f.readlines()
+
+	for line in lines:
+		if line == lines[0]:
+			alphabet_size = int(line.rstrip())
+		else:
+			current_line = line.rstrip().replace(" ", "")
+			row = []
+			for char in current_line:
+				if char == current_line[0]:
+					alphabet.append(char)
+				else:
+					row.append(int(char))
+			scoring_matrix.append(row)
+
+	return (alphabet_size, alphabet, np.array(scoring_matrix))
 
 
-def parse_arguments():
-    """ Parse the necessary arguments, otherwise use default """
-    global cost, gap_cost, alphabet
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-s", help = "File that holds all sequences that are to be aligned")
-    parser.add_argument("-c", help = "Txt file defining the cost matrix in a Phylip-like format")
-    args = parser.parse_args()
-    cost_file = args.c
-    sequence_file = args.s
 
-    if sequence_file is None:
-        print_usage()
-        sys.exit(1)
+# Optimal linear alignment of 2 sequences
+def align_linear(seq1, seq2):
+	n=len(seq1)+1
+	m=len(seq2)+1
+	S= np.zeros((n,m))
+	for j in range(0,m):
+		for i in range(0,n):
+			if i==0 and j==0:
+				S[i,j]=0
+			elif i==0:
+				S[i,j]=S[i,j-1]+gap_cost
+			elif j==0:
+				S[i,j]=S[i-1,j]+gap_cost
+			else:
+				S[i,j]=min(S[i-1,j]+gap_cost,
+						   S[i,j-1]+gap_cost,
+						   S[i-1,j-1]+cost[look_up[seq1[i-1]], look_up[seq2[j-1]]])
 
-    if cost_file is None:
-        print("Using default values:")
-        print("Gapcost:", gap_cost)
-        print("Subcost:\n", cost, "\n")
-    else:
-        gap_cost, cost, alphabet = read_in_scoring_matrix(cost_file)
+	return S
 
-    return sequence_file
+# Traceback algorithm - pairwise linear alignment
+def backtrack(matrix, seq1, seq2):
+	i=len(seq1)
+	j=len(seq2)
+	align1 = ""
+	align2 = ""
+	while i>0 or j>0:
+		if matrix[i,j] == matrix[i-1,j-1]+cost[look_up[seq1[i-1]], look_up[seq2[j-1]]]:
+			align1 = str(seq1[i-1]) + align1
+			align2 = str(seq2[j-1]) + align2
+			i-=1
+			j-=1
+		elif matrix[i,j] == matrix[i-1,j]+gap_cost:
+			align1 = str(seq1[i-1]) + align1
+			align2 = "-" + align2
+			i -=1
+		else:
+			align2 = str(seq2[j-1]) + align2
+			align1 = "-" + align1
+			j -=1
 
+	return [align1, align2]
 
-def read_in_sequences(file):
-    """ Read in a fasta file containing sequences """
-    sequences = []
-    for seq_record in SeqIO.parse(file, "fasta"):
-        sequences.append(seq_record.seq.upper())
+## Merging of alignments
+def merge_alignment(seq1, seq2, alignment):
+	num1 = []
+	num2 = []
+	a = len(alignment)
+	alignment.append([])
+	for i in seq1[0]:
+		num1.append(str(i))
+	for j in seq2[0]:
+		num2.append(str(j))
+	i = 0
+	j = 0
+	k = 0
+	while k < len(alignment[0]) or i < len(num1):
+		if k >= len(alignment[0]):
+			alignment[-1].append(num2[j])
+			for b in range(0,a):
+				alignment[b].append("-")
+			i += 1
+			j += 1
+			k += 1
+		elif i >= len(num1):
+			alignment[-1].append("-")
+			k +=1
+		elif num1[i] == alignment[0][k]:
+			alignment[-1].append(num2[j])
+			i += 1
+			j += 1
+			k += 1
+		elif num1[i] == "-":
+			alignment[-1].append(num2[j])
+			for b in range(0,a):
+				alignment[b].insert(k,"-")
+			i +=1
+			j +=1
+			k +=1
+		elif alignment[0][k] == "-":
+			alignment[-1].append("-")
+			k +=1
+	return alignment
 
-    return sequences
+## Find center string
+def find_center(seqs):
+	n = len(seqs)
+	S= np.zeros((n,n))
+	for i in range(0,n):
+		for j in range(0,n):
+			a = align_linear(seqs[i],seqs[j])[-1,-1]
+			S[i,j] = a
+	sum_val = []
+	sum_val.append(np.sum(S,axis=1))
+	return np.argmin(sum_val)
 
+## Combining the different functions
+def combine(list_of_seqs):
+	center = find_center(list_of_seqs)
+	list_of_seqs[0], list_of_seqs[center] = list_of_seqs[center], list_of_seqs[0]
+	alignment = [[]]
+	stored_alignments = []
+	for i in list_of_seqs[0]:
+		alignment[0].append(i)
+	for i in range(1,len(list_of_seqs)):
+		matrix = align_linear(list_of_seqs[0],list_of_seqs[i])
+		pair_align = backtrack(matrix,list_of_seqs[0],list_of_seqs[i])
+		stored_alignments.append(pair_align)
+		new = merge_alignment([pair_align[0]], [pair_align[1]],alignment)
+		alignment = new
+	return alignment, stored_alignments
 
-def read_in_scoring_matrix(file):
-    """ Read in the alphabet, scoring matrix and gap cost """
-    alphabet = []
-    scoring_matrix = []
-
-    with open(file, "r") as f:
-        lines = f.readlines()
-
-    firstLine = True
-    for line in lines:
-        line_arr = line.rstrip().split()
-        if firstLine == True:
-            gapcost = int(line_arr[0])
-            firstLine = False
-        else:
-            row = []
-            for char in line_arr:
-                if char == line_arr[0]: # add char to alphabet
-                    alphabet.append(char)
-                else:                   # add cost to scoring matrix
-                    row.append(int(char))
-            scoring_matrix.append(row)
-
-    return gapcost, np.array(scoring_matrix), alphabet
-
-
-def levenshtein_distance(seq1, seq2):
-    """ Calculate the levenshtein distance between two sequences """
-    n, m = len(seq1) + 1, len(seq2) + 1
-    D = np.zeros((n, m), dtype=int)
-
-    for i in range(n):
-        for j in range(m):
-            if i == 0 and j == 0:
-                D[i, j] = 0
-            elif i == 0:
-                D[i, j] = j
-            elif j == 0:
-                D[i, j] = i
-            else:
-                indicator = 1
-                if seq1[i - 1] == seq2[j - 1]:
-                    indicator = 0
-                D[i, j] = min(D[i - 1, j - 1] + indicator,
-                              D[i, j - 1] + 1,
-                              D[i - 1, j] + 1)
-
-    return D[n - 1, m - 1]
-
-
-def determine_center_sequence(sequences):
-    """ Determine the center sequence such that the sum of the levenshtein distance is minimized """
-    summed_distances = []
-    for seq in sequences:
-        sum = 0
-        for seq_ in sequences:
-            sum += levenshtein_distance(seq, seq_)
-        summed_distances.append(sum)
-
-    min = summed_distances[0]
-    index = 0
-    for i in range(1, len(sequences)):
-        if min >= summed_distances[i]:
-            min = summed_distances[i]
-            index = i
-
-    return index, sequences[index]
-
-
-def optimal_alignment(seq1, seq2):
-    """ Calculate the pairwise optimal alignment between two sequences using linear gapcost """
-    n, m = len(seq1) + 1, len(seq2) + 1
-    align_matrix = np.zeros((n, m))
-    for i in range(n):
-        for j in range(m):
-            if j == 0 and i == 0:
-                align_matrix[i, j] = 0
-            elif j == 0:
-                align_matrix[i, j] = align_matrix[i-1, j] + gap_cost
-            elif i == 0:
-                align_matrix[i, j] = align_matrix[i, j-1] + gap_cost
-            else:
-                align_matrix[i, j] = min(
-                    align_matrix[i-1, j-1] + cost[look_up[seq1[i-1]], look_up[seq2[j-1]]],
-                    align_matrix[i-1, j] + gap_cost,
-                    align_matrix[i, j-1] + gap_cost
-                )
-
-    return align_matrix
-
-
-def traceback(opt, seq1, seq2):
-    """ Traceback the optimal alignment of two sequences """
-    s1 = ""
-    s2 = ""
-    i, j = len(seq1), len(seq2)
-    while i >= 0 and j >= 0:
-        if opt[i, j] == opt[i-1, j-1] + cost[look_up[seq1[i-1]], look_up[seq2[j-1]]]:
-            s1 += seq1[i-1]
-            s2 += seq2[j-1]
-            i -= 1
-            j -= 1
-        elif opt[i,j] == opt[i-1, j] + gap_cost:
-            s1 += seq1[i-1]
-            s2 += "-"
-            i -= 1
-        elif opt[i,j] == opt[i, j-1] + gap_cost:
-            s1 += "-"
-            s2 += seq2[j-1]
-            j -= 1
-        elif i == 0:
-            while j >= 0:
-                if j > 0:
-                    s1 += ("-")
-                j -= 1
-        elif j == 0:
-            while i >= 0:
-                if i > 0:
-                    s2 += ("-")
-                i -= 1
-
-    return s1[::-1], s2[::-1]
+## Find the score of the alignment
+def compute_sp_score(alignment):
+	score = 0
+	for i in range(len(alignment)):
+		for j in range(i+1, len(alignment)):
+			for c in range(len(alignment[i])):
+				score = score +cost[look_up[alignment[i][c]], look_up[alignment[j][c]]]
+	return score
 
 
-def extendExistingRow(current_row, new_gaps):
-    """ Introduce new gaps into the existing rows of the matrix M """
-    new_row = []
-    for i in range(len(current_row)):
-        if i in new_gaps:
-            for gaps in range(new_gaps[i]):
-                new_row.append("-")
-        new_row.append(current_row[i])
+def measure_times():
+	""" Use this method to measure times of the algorithms using sequences of different length """
+	sequences = []
+	sequence_length = [5, 10, 50, 100, 500, 1000, 2500, 5000, 7500, 10000] # edit sequence
 
-    return np.array(new_row)
+	for seq_record in SeqIO.parse("sequences/rand_sequences.fasta", "fasta"):
+		sequences.append(seq_record.seq.upper())
 
+	times_opt = []
+	times_back = []
+	i = 0
+	while i < len(sequences):
+		# measure optimal alignment algorithm
+		start_opt = time.time()
+		opt = optimal_alignment(sequences[i], sequences[i + 1])
+		end_opt = time.time()
+		times_opt.append(end_opt - start_opt)
 
-def extendMSAMatrix(OPT, M):
-    """ Extend the multiple alignment matrix M with a new optimal alignment OPT """
-    extendedM = []
+		# measure trace back algorithm
+		start_back = time.time()
+		aligned = traceback(opt, sequences[i], sequences[i + 1])
+		end_back = time.time()
+		times_back.append(end_back - start_back)
+		i += 2
 
-    i, j = 0, 0
-    gap_index = 0
-    new_gaps = {}
-    new_row = [] # row that inserts the new sequence in the matrix M
-
-    while i < len(M[0]):
-        if OPT[0][j] == "-":
-            new_row.append(OPT[1][j])
-            if i not in new_gaps: # introduce a gap at position i in the matrix
-                new_gaps[i] = 1
-            else:
-                new_gaps[i] += 1 # extend an existing gap at position i
-            j += 1
-        elif M[0][i] == "-":
-            new_row.append("-")
-            i += 1
-        elif M[0][i] == OPT[0][j]:
-            new_row.append(OPT[1][j])
-            j += 1
-            i += 1
-
-    for row in M:
-        extendedM.append(extendExistingRow(row, new_gaps))
-
-    extendedM.append(np.array(new_row))
-    return np.array(extendedM)
+	return times_opt, times_back
 
 
-def pretty_print_M(M):
-    """ Print the content of M as a string """
-    print("MSA")
-    print("=======================================================")
-    for row in M:
-        string = ""
-        for char in row:
-            string += char
-        print(string)
+def test_alignment_algorithm():
+	""" Test the alignment algorithm by using the sequences from the project2_examples.txt """
+	sequences = []
+
+	for seq_record in SeqIO.parse("sequences/test_sequences.fasta", "fasta"):
+		sequences.append(seq_record.seq)
+
+	i = 0
+	while i < len(sequences):
+		opt = optimal_alignment(sequences[i], sequences[i + 1])
+		print("\nScore of the optimal global optimal_alignment: ", opt[len(sequences[i + 1]), len(sequences[i])])
+
+		aligned = traceback(opt, sequences[i], sequences[i + 1])
+		print(aligned[0], "\n" + aligned[1], "\n")
+		i += 2
 
 
 def verify_MSA(M, alignments):
-    """ Get the pairwise alignments from the MSA matrix by deleting gap columns.
-        Compare the alignments to the optimal alignments that have been calculated before """
-    s1 = ""
-    s2 = ""
-    count_alignments = 0
-    verified = True
-    for i in range(1, len(M)):
-        for j in range(len(M[i])):
-            if not (M[0][j] == "-" and M[i][j] == "-"): # M[0] is center sequence
-                s1 += M[0][j]
-                s2 += M[i][j]
-        if alignments[count_alignments][0] != s1:
-            verified = False
-            print("\n" + alignments[count_alignments][0] + "\n" + s1 + "\n")
-        if alignments[count_alignments][1] != s2:
-            verified = False
-            print(alignments[count_alignments][1] + "\n" + s2 + "\n")
+	s1 = ""
+	s2 = ""
+	count_alignments = 0
 
-        count_alignments += 1
-        s1 = ""
-        s2 = ""
+	verified = True
+	for i in range(1, len(M)):
+		for j in range(len(M[i])):
+			if not (M[0][j] == "-" and M[i][j] == "-"): # M[0] is center sequence
+				s1 += M[0][j]
+				s2 += M[i][j]
+		if alignments[count_alignments][0] != s1 or alignments[count_alignments][1] != s2:
+			verified = False
+			print(alignments[count_alignments])
+			print((s1, s2))
 
-    return verified
+		count_alignments += 1
+		s1 = ""
+		s2 = ""
+
+	return verified
 
 
 def main():
-    sequence_file = parse_arguments()
-    print("Reading sequences from", sequence_file, "\n")
+	global gap_cost, cost, file
+	# Arg parse
+	parser = argparse.ArgumentParser()
+	parser.add_argument("Seqs", help = "Fasta file path for sequences")
+	parser.add_argument("--P", help = "Output strings of aligned sequences: Write Y")
+	parser.add_argument("--C", help = "txt file with Phylip-like format")
+	parser.add_argument("--G", help = "Gap cost")
+	args = parser.parse_args()
+	seqs = args.Seqs
+	if args.G != None:
+		gap_cost = np.array([args.G])
+		gap_cost = gap_cost.astype("int32")[0]
 
-    # sequences = ["ATTCT", "ACGT", "CTCGA", "ACGGT"]
-    sequences = read_in_sequences(sequence_file)
-    center_index, center_sequence = determine_center_sequence(sequences)
-    print("Determined center sequence:", center_sequence, "\n")
+	print_statement = args.P
 
-    alignments = [] # save all alignments in order to verify later
+	file = args.C
 
-    M = None
-    for i in range(len(sequences)):
-        if i != center_index: # do not calculate optimal alignment between the center sequence and itself
-            opt = optimal_alignment(sequences[center_index], sequences[i])
-            alignment = traceback(opt, sequences[center_index], sequences[i])
-            alignments.append(alignment)
 
-            if M is not None:
-                M = extendMSAMatrix(alignment, M)
-            else:   # set M to the first optimal alignment
-                M = np.array([alignment[0], alignment[1]])
+	if print_statement is not None:
+		print_statement = print_statement.lower() # lower case letters
 
-    pretty_print_M(M)
-    if verify_MSA(M, alignments):
-        print("\nMSA verified! :-)")
-    else:
-        print("\nMSA could not be verified! :-(")
+	if file is not None:
+		alphabet_size, alphabet, cost = read_in_scoring_matrix()
+		look_up = dict()
+		for i in range(alphabet_size):
+			look_up[alphabet[i].upper()] = i
+
+	### Running the Code ###
+	seq_list = []
+	for seq_record in SeqIO.parse(seqs, "fasta"):
+		seq_list.append((seq_record.seq.upper()))
+
+
+	start_opt = time.time()
+	opt, alignments = combine(seq_list)
+	end_opt = time.time()
+
+	verify = verify_MSA(opt, alignments)
+	if verify:
+		print("Verified.")
+	else:
+		print("No verified.")
+
+	op1 = []
+	for st in opt:
+		coll = ""
+		for base in st:
+			coll = coll + base
+		op1.append(coll)
+	print("\nAlignment: ")
+	for s in op1:
+		print(s)
+	print("Making the optimal alignment took", end_opt - start_opt, "seconds.\n")
+
+	start_back = time.time()
+	aligned = compute_sp_score(opt)
+	end_back = time.time()
+
+	if print_statement == "y":
+		print("The score of the alignment: ", aligned)
+		print("Calculating the score", end_back - start_back, "seconds.\n")
+
+	### Writing the aligned sequences into a fasta file ###
+	#record = SeqRecord(Seq(opt),id="Seq1")
+
+	#result_file = "result_linear.fasta"
+	#SeqIO.write([record], result_file, "fasta")
+	#print("Wrote results in fasta format to " +  result_file + ".")
+
+	# test alignment algorithm using examples
+	#test_alignment_algorithm()
+
+	# function for taking the performance measures
+	# times_opt, times_back = measure_times()
+	# print("Optimal alignment:", times_opt)
+	# print("Traceback", times_back)
 
 
 if __name__ == '__main__':
-    main()
+	main()
---
+++ sp_approx_jana.py
@@ -0,0 +1,304 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# python3 2_approximation_algorithm.py -c scoring_matrix.txt -s testdata_short.fasta
+
+
+import argparse
+import numpy as np
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+# Default values
+cost = np.array([
+    # A  C  G  T
+    [0, 5, 2, 5],
+    [5, 0, 5, 2],
+    [2, 5, 0, 5],
+    [5, 2, 5, 0]])
+gap_cost = 5
+alphabet = ["A", "C", "G", "T"]
+look_up = {"A": 0, "C": 1, "G": 2, "T": 3, "N": 0, "R": 0, "S": 0}
+
+
+def print_usage():
+    """ Define a usage message """
+    print("""
+          Usage: sp_approx.py -s <sequence_file> -c <cost_file>\n
+          where <sequence_file> contains a set of sequences over the alphabet
+          {A,C,G,T,-} in FASTA format.
+          And <cost_file> contains a cost matrix and a gap cost in a phylip-like
+          format. If -c is not specified, the default values are used.
+          """)
+
+
+def parse_arguments():
+    """ Parse the necessary arguments, otherwise use default """
+    global cost, gap_cost, alphabet
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", help = "File that holds all sequences that are to be aligned")
+    parser.add_argument("-c", help = "Txt file defining the cost matrix in a Phylip-like format")
+    args = parser.parse_args()
+    cost_file = args.c
+    sequence_file = args.s
+
+    if sequence_file is None:
+        print_usage()
+        sys.exit(1)
+
+    if cost_file is None:
+        print("Using default values:")
+        print("Gapcost:", gap_cost)
+        print("Subcost:\n", cost, "\n")
+    else:
+        gap_cost, cost, alphabet = read_in_scoring_matrix(cost_file)
+
+    return sequence_file
+
+
+def read_in_sequences(file):
+    """ Read in a fasta file containing sequences """
+    sequences = []
+    for seq_record in SeqIO.parse(file, "fasta"):
+        sequences.append(seq_record.seq.upper())
+
+    return sequences
+
+
+def read_in_scoring_matrix(file):
+    """ Read in the alphabet, scoring matrix and gap cost """
+    alphabet = []
+    scoring_matrix = []
+
+    with open(file, "r") as f:
+        lines = f.readlines()
+
+    firstLine = True
+    for line in lines:
+        line_arr = line.rstrip().split()
+        if firstLine == True:
+            gapcost = int(line_arr[0])
+            firstLine = False
+        else:
+            row = []
+            for char in line_arr:
+                if char == line_arr[0]: # add char to alphabet
+                    alphabet.append(char)
+                else:                   # add cost to scoring matrix
+                    row.append(int(char))
+            scoring_matrix.append(row)
+
+    return gapcost, np.array(scoring_matrix), alphabet
+
+
+def levenshtein_distance(seq1, seq2):
+    """ Calculate the levenshtein distance between two sequences """
+    n, m = len(seq1) + 1, len(seq2) + 1
+    D = np.zeros((n, m), dtype=int)
+
+    for i in range(n):
+        for j in range(m):
+            if i == 0 and j == 0:
+                D[i, j] = 0
+            elif i == 0:
+                D[i, j] = j
+            elif j == 0:
+                D[i, j] = i
+            else:
+                indicator = 1
+                if seq1[i - 1] == seq2[j - 1]:
+                    indicator = 0
+                D[i, j] = min(D[i - 1, j - 1] + indicator,
+                              D[i, j - 1] + 1,
+                              D[i - 1, j] + 1)
+
+    return D[n - 1, m - 1]
+
+
+def determine_center_sequence(sequences):
+    """ Determine the center sequence such that the sum of the levenshtein distance is minimized """
+    summed_distances = []
+    for seq in sequences:
+        sum = 0
+        for seq_ in sequences:
+            sum += levenshtein_distance(seq, seq_)
+        summed_distances.append(sum)
+
+    min = summed_distances[0]
+    index = 0
+    for i in range(1, len(sequences)):
+        if min >= summed_distances[i]:
+            min = summed_distances[i]
+            index = i
+
+    return index, sequences[index]
+
+
+def optimal_alignment(seq1, seq2):
+    """ Calculate the pairwise optimal alignment between two sequences using linear gapcost """
+    n, m = len(seq1) + 1, len(seq2) + 1
+    align_matrix = np.zeros((n, m))
+    for i in range(n):
+        for j in range(m):
+            if j == 0 and i == 0:
+                align_matrix[i, j] = 0
+            elif j == 0:
+                align_matrix[i, j] = align_matrix[i-1, j] + gap_cost
+            elif i == 0:
+                align_matrix[i, j] = align_matrix[i, j-1] + gap_cost
+            else:
+                align_matrix[i, j] = min(
+                    align_matrix[i-1, j-1] + cost[look_up[seq1[i-1]], look_up[seq2[j-1]]],
+                    align_matrix[i-1, j] + gap_cost,
+                    align_matrix[i, j-1] + gap_cost
+                )
+
+    return align_matrix
+
+
+def traceback(opt, seq1, seq2):
+    """ Traceback the optimal alignment of two sequences """
+    s1 = ""
+    s2 = ""
+    i, j = len(seq1), len(seq2)
+    while i >= 0 and j >= 0:
+        if opt[i, j] == opt[i-1, j-1] + cost[look_up[seq1[i-1]], look_up[seq2[j-1]]]:
+            s1 += seq1[i-1]
+            s2 += seq2[j-1]
+            i -= 1
+            j -= 1
+        elif opt[i,j] == opt[i-1, j] + gap_cost:
+            s1 += seq1[i-1]
+            s2 += "-"
+            i -= 1
+        elif opt[i,j] == opt[i, j-1] + gap_cost:
+            s1 += "-"
+            s2 += seq2[j-1]
+            j -= 1
+        elif i == 0:
+            while j >= 0:
+                if j > 0:
+                    s1 += ("-")
+                j -= 1
+        elif j == 0:
+            while i >= 0:
+                if i > 0:
+                    s2 += ("-")
+                i -= 1
+
+    return s1[::-1], s2[::-1]
+
+
+def extendExistingRow(current_row, new_gaps):
+    """ Introduce new gaps into the existing rows of the matrix M """
+    new_row = []
+    for i in range(len(current_row)):
+        if i in new_gaps:
+            for gaps in range(new_gaps[i]):
+                new_row.append("-")
+        new_row.append(current_row[i])
+
+    return np.array(new_row)
+
+
+def extendMSAMatrix(OPT, M):
+    """ Extend the multiple alignment matrix M with a new optimal alignment OPT """
+    extendedM = []
+
+    i, j = 0, 0
+    gap_index = 0
+    new_gaps = {}
+    new_row = [] # row that inserts the new sequence in the matrix M
+
+    while i < len(M[0]):
+        if OPT[0][j] == "-":
+            new_row.append(OPT[1][j])
+            if i not in new_gaps: # introduce a gap at position i in the matrix
+                new_gaps[i] = 1
+            else:
+                new_gaps[i] += 1 # extend an existing gap at position i
+            j += 1
+        elif M[0][i] == "-":
+            new_row.append("-")
+            i += 1
+        elif M[0][i] == OPT[0][j]:
+            new_row.append(OPT[1][j])
+            j += 1
+            i += 1
+
+    for row in M:
+        extendedM.append(extendExistingRow(row, new_gaps))
+
+    extendedM.append(np.array(new_row))
+    return np.array(extendedM)
+
+
+def pretty_print_M(M):
+    """ Print the content of M as a string """
+    print("MSA")
+    print("=======================================================")
+    for row in M:
+        string = ""
+        for char in row:
+            string += char
+        print(string)
+
+
+def verify_MSA(M, alignments):
+    """ Get the pairwise alignments from the MSA matrix by deleting gap columns.
+        Compare the alignments to the optimal alignments that have been calculated before """
+    s1 = ""
+    s2 = ""
+    count_alignments = 0
+    verified = True
+    for i in range(1, len(M)):
+        for j in range(len(M[i])):
+            if not (M[0][j] == "-" and M[i][j] == "-"): # M[0] is center sequence
+                s1 += M[0][j]
+                s2 += M[i][j]
+        if alignments[count_alignments][0] != s1:
+            verified = False
+            print("\n" + alignments[count_alignments][0] + "\n" + s1 + "\n")
+        if alignments[count_alignments][1] != s2:
+            verified = False
+            print(alignments[count_alignments][1] + "\n" + s2 + "\n")
+
+        count_alignments += 1
+        s1 = ""
+        s2 = ""
+
+    return verified
+
+
+def main():
+    sequence_file = parse_arguments()
+    print("Reading sequences from", sequence_file, "\n")
+
+    # sequences = ["ATTCT", "ACGT", "CTCGA", "ACGGT"]
+    sequences = read_in_sequences(sequence_file)
+    center_index, center_sequence = determine_center_sequence(sequences)
+    print("Determined center sequence:", center_sequence, "\n")
+
+    alignments = [] # save all alignments in order to verify later
+
+    M = None
+    for i in range(len(sequences)):
+        if i != center_index: # do not calculate optimal alignment between the center sequence and itself
+            opt = optimal_alignment(sequences[center_index], sequences[i])
+            alignment = traceback(opt, sequences[center_index], sequences[i])
+            alignments.append(alignment)
+
+            if M is not None:
+                M = extendMSAMatrix(alignment, M)
+            else:   # set M to the first optimal alignment
+                M = np.array([alignment[0], alignment[1]])
+
+    pretty_print_M(M)
+    if verify_MSA(M, alignments):
+        print("\nMSA verified! :-)")
+    else:
+        print("\nMSA could not be verified! :-(")
+
+
+if __name__ == '__main__':
+    main()
